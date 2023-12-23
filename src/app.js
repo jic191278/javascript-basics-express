3c867674b3ef4b87ebcb3bfb3a3a96c0f58d2436
James Carrier <jic191278@gmail.com>
2023-12-23 08:04:09 +0000

    complete errors and validation

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 const stringsRoute = require('./routes/strings.route');
+const numbersRoute = require('./routes/numbers.route');
 
 app.use('/strings', stringsRoute);
+app.use('/numbers', numbersRoute);
 
 module.exports = app;
---
+++ src/routes/numbers.route.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const router = express.Router();
+
+const { add, subtract } = require('../lib/numbers');
+
+router.get('/add/:a/and/:b', (req, res) => {
+  const a = parseInt(req.params.a, 10);
+  const b = parseInt(req.params.b, 10);
+
+  return Number.isNaN(a) || Number.isNaN(b)
+    ? res.status(400).send({ error: 'Parameters must be valid numbers.' })
+    : res.status(200).send({ result: add(a, b) });
+});
+
+router.get('/subtract/:a/from/:b', (req, res) => {
+  const a = parseInt(req.params.a, 10);
+  const b = parseInt(req.params.b, 10);
+
+  return Number.isNaN(a) || Number.isNaN(b)
+    ? res.status(400).send({ error: 'Parameters must be valid numbers.' })
+    : res.status(200).send({ result: subtract(b, a) });
+});
+
+module.exports = router;
